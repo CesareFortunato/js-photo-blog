ef93645086ec7cdb58fc7c7863a5710282c1cbce
Cesare89 <cf@nume.plus>
2026-01-09 12:58:30 +0100

    feat: setup open and close cards using event delegation

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ const polaroid = document.querySelector("card");
 const cardImgContainer = document.querySelector(".card .img-container");
 const captionEl = document.querySelector(".card .caption");
 
+//variabili overlay per event
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("overlay-close");
+
 
 
 // chiamata all'api
 
 axios.get("https://lanciweb.github.io/demo/api/pictures/")
-    .then(res => {
+  .then(res => {
     generateCards(res.data);
   })
   .catch(console.error);
 
 
 
-  //funzioni
+//funzioni
 
-  function generateCards(polaroids) {
+function generateCards(polaroids) {
   let outputPolaroids = "";
 
   polaroids.forEach(polaroid => {
@@ -38,3 +42,20 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
 
   mainContainer.innerHTML = outputPolaroids;
 }
+
+//event per aprire polaroid
+
+
+mainContainer.addEventListener("click", (e) => {
+  const card = e.target.closest(".card");   //.target -> riconosce l'elemento HTML su cui ho cliccato / .closest(".card") risale all'elemento più vicino che ha come classe .card
+  if (!card) return;          // se click fuori dalle card
+
+  overlay.classList.remove("hidden");
+});
+
+//chiusura polaroid
+
+closeBtn.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+});
+
